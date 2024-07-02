b06b0cd8491b217b8ae0a60f72e7d924b0d72e3c
muhammadjon8 <umuhammadjon22@gmail.com>
2024-07-02 21:47:43 +0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>INdex file</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      container: {
+        padding: {
+          default: "1rem",
+          sm: "1.5rem",
+          md: "2rem",
+          lg: "2.5rem",
+          xl: "3rem",
+        },
+        center: true,
+      },
+    },
+  },
+  plugins: [],
+};
